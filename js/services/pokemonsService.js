6f2e5fe0e29421d2d28a7dd18fdcef581a048b1f
Gabriel Panassol <gpanassol@gmail.com>
2017-12-11 20:50:59 -0200

    Corrigido erro na service

--- js/services/pokemonsService.js
+++ js/services/pokemonsService.js
@@ -2,9 +2,9 @@ angular.module('pokemons').factory('pokemonsService', function($http){
 
 	var _getPokemons = function(){
 		return $http.get("http://pokeapi.salestock.net/api/v2/");
-	}
+	};
 
 	return {
-		getPokemons = _getPokemons
+		getPokemons: _getPokemons
 	};
 });
